09320f7352b35f682c2e4bbb35b21a47a549962a
yanasavitsky <89624578+yanasavitsky@users.noreply.github.com>
2021-12-07 21:35:38 -0800

    js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 var canvas;
 
 function setup() {
-  canvas = createCanvas(400, 400);
+  canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
+  canvas.style("z-index", "-1");
 }
 
 function draw() {
